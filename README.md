133348481202ce4d4bdcb95b980e5454655df091
yilian <liub@reed.edu>
2018-10-31 18:16:57 -0700

    On branch master  Changes to be committed: 	modified:   README.md modified:   reverse.cc
     Untracked files:
    	a.out

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hw5extra
 hw5 extra credit assignment
+
+the main function tests both functions from list of length 0 to 5 
+and calls the test function to print out the reversed list by the 
+iterative function and the twice reversed list by the recursive function.
--- reverse.cc
+++ reverse.cc
@@ -21,29 +21,65 @@ void print_list(const LNode* head)
 LNode*
 reverse_list(LNode* head)
 {
-	if(!head)		return head;
-	if(!(head->next_)) 	return head;
-	LNode* newHead = head->next;
-	while(!(head->next_->next))
+	LNode* newHead = nullptr;
+	LNode* prevHead = nullptr;
+	while(head != nullptr)
 	{
-		newHead = head->next_->next_;
-		head->next_->next_ = head;
-
+		newHead = head->next_;
+		head->next_ = prevHead;
+		prevHead = head;
+		head = newHead;
+	}
+	return prevHead;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 // Reverse a list recursively
-LNode*
-reverse_list_r(LNode* head)
-{	
-	if(!head)		return head;
+LNode*	reverse_list_r(LNode* head)
+{
+	if(!head) 	return head;
 	if(!(head->next_))	return head;
-	reverse_list_r(head)->next_ = head;
-	return head;
+	LNode* newHead = reverse_list_r(head->next_);
+	head->next_->next_ = head;
+	head->next_ = nullptr;
+	return newHead;
+}
+
+void test(LNode* head)
+{
+	std::cout<<"original list:"<<"\n";
+	print_list(head);
+	std::cout<<"\n";
+		
+	std::cout<<"iterated list:"<<"\n";
+	head = reverse_list(head);
+	print_list(head);
+	
+	std::cout<<"\n";
+	std::cout<<"recursively reversed list:"<<"\n";
+	head = reverse_list_r(head);
+	print_list(head);
+	std::cout<<"\n"<<"\n";
 }
 
 ////////////////////////////////////////////////////////////////////////////// 
 int main()
 {
-...
+	
+	LNode* head = new LNode{0, nullptr};
+	test(head);
+	head = nullptr;
+	test(head);	
+	for (int listLen = 1; listLen < 5; listLen ++)
+	{
+		head =new LNode{ 0,new LNode{1, nullptr}};
+		LNode* current = head->next_;
+		for(int i = 2; i<=listLen ; i++)
+		{
+			current->next_ = new LNode{i,nullptr};
+			current = current->next_;
+		}
+		test(head);
+		}
+
 }
